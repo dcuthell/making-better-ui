36a128747b8284bf64549f60b0b5c3722fcfa2a3
dcuthell <dcc1447@gmail.com>
2017-10-01 15:52:46 -0700

    update log, add background images to each page

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,3 @@
 $(document).ready(function(){
-  $("#enter-button").click(function(event) {
-    event.preventDefault();
-    $('body').css('background-image', 'url("./img/pic4.jpg")');
-  });
+  
 });
